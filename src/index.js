a49a7ad7f31b93a4eaccb2bf66a73c8edc95f04f
Kinskaya <kinskaya.nastya@gmail.com>
2021-02-19 19:34:32 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (numbers) {
+    if (numbers === undefined || numbers.length === 0) {
+        return 0;
+    }
+    let minNum = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] < minNum) {
+            minNum = numbers[i];
+        }
+    }
+    return minNum;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (numbers) {
+    if (numbers === undefined || numbers.length === 0) {
+        return 0;
+    }
+    let maxNum = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > maxNum) {
+            maxNum = numbers[i];
+        }
+    }
+    return maxNum;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (numbers) {
+    if (numbers === undefined || numbers.length === 0) {
+        return 0;
+    }
+    let len = numbers.length;
+    let sum = numbers.reduce((acc, curr) => acc + curr, 0);
+    return (sum / len);
 }
